d4b93973fbcd65e1d5df42727dc23e433b465df1
pramod <pramod.152002@gmail.com>
2023-07-18 12:37:02 +0530

    comment connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const dotenv  = require('dotenv');
-const morgan  = require('morgan');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const ErrorHandler = require('./middleware/error');
@@ -13,29 +13,29 @@ app.use(express.json());
 app.use(CookieParser());
 
 //load dotenv vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 //connect db
 
-connectDB();
+//connectDB();
 
 //routes files
 const bootcamps = require('./routes/bootcamps');
-const auth     = require('./routes/auth');
-const course   = require('./routes/course');
+const auth = require('./routes/auth');
+const course = require('./routes/course');
 
 //const logger = require('./middleware/logger');
 //dev logging middleware
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     console.log('morgan middleware');
     app.use(morgan('dev'));
 }
-app.get('/api',(req,res)=>{
+app.get('/api', (req, res) => {
     res.send('Congratulations, NodeJs is working properlly')
 })
-app.use('/api/v1/bootcamps',bootcamps)
-app.use('/api/v1/auth',auth);
-app.use('/api/v1/courses',course);
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/courses', course);
 app.use(ErrorHandler);
 
 PORT = process.env.PORT || 5000;
@@ -45,8 +45,8 @@ const server = app.listen(
     console.log(`server running in ${process.env.NODE_ENV} MODE on port ${PORT}`.yellow.bold)
 );
 
-process.on('unhandledRejection',(err,promise)=>{
+process.on('unhandledRejection', (err, promise) => {
     console.log(`unhandled rejection ${err.message}`);
     //close server and exit process
-    server.close(()=>process.exit(1));    
+    server.close(() => process.exit(1));
 })
